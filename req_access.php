6c6f59256d2392e482dcd7cb7dd1eddbe2209f4a
Blair Sharpe <Bsharpe3839@conestogac.on.ca>
2018-06-04 14:31:39 -0400

    Update variables to POST

--- req_access.php
+++ req_access.php
@@ -1,13 +1,10 @@
 <?php
-$link = mysqli_connect("localhost", "root", "", "demo");
+$link = mysqli_connect("localhost", "root", "", "userinfo");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$firstname = $_POST["firstname"];
-$lastname = $_POST["lastname"];
-$email= $_POST["email"];
-$sql = "INSERT INTO info (first_name, last_name, email) VALUES ('$firstname', '$lastname', '$email')";
-
+$sql = "INSERT INTO userinfo (firstname, lastname, email) VALUES 
+    ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]')";
 if(mysqli_query($link, $sql)){
     echo "Records inserted successfully.";
 } else{
