91473de62def321e8d0f20a341a4f800982130ec
dylankite <dylan44117@gmail.com>
2018-07-17 17:50:49 +0000

    User authentication hard coded

--- php/login.php
+++ php/login.php
@@ -4,25 +4,12 @@ $errorMsg = "";
 $validUser = $_SESSION["login"] === true;
 if(isset($_POST["sub"])) {
 $validUser = $_POST["username"] == "admin" && $_POST["password"] == "password";
+echo $_POST['username'];
 if(!$validUser) $errorMsg = "Invalid username or password.";
-else $_SESSION["login"] = true;
+else {
+	$_SESSION["login"] = true; 
+	header('Location: ../index.html');
 }
-if($validUser) {
-header("Location: /login-success.php"); die();
 }
+echo $errorMsg;
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-		<title>Login</title>
-	</head>
-	<body>
-		<form name="input" action="" method="post">
-			<label for="username">Username:</label><input type="text" value="<?= $_POST["username"] ?>" id="username" name="username" />
-			<label for="password">Password:</label><input type="password" value="" id="password" name="password" />
-			<div class="error"><?= $errorMsg ?></div>
-			<input type="submit" value="Home" name="sub" />
-		</form>
-	</body>
-</html>
