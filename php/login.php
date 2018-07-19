04913f0e20a840a5d67ec66ecbbbb7e37b4c13d2
dylankite <dylan44117@gmail.com>
2018-07-19 14:36:02 +0000

    User Auth with the database on index

--- php/login.php
+++ php/login.php
@@ -1,15 +1,36 @@
 <?php
 session_start();
 $errorMsg = "";
-$validUser = $_SESSION["login"] === true;
+$validUser = True;
+
 if(isset($_POST["sub"])) {
-$validUser = $_POST["username"] == "admin" && $_POST["password"] == "password";
-echo $_POST['username'];
-if(!$validUser) $errorMsg = "Invalid username or password.";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$link = mysqli_connect("localhost", "ese","ese");
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+$sql = "select * from elevator.users where
+    email = '$email' and password='$password'";
+$result = mysqli_query($link, $sql);
+if(!$result){
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+if($result->num_rows==0){
+	$validUser = False;
+}else{
+	$validUser = True;
+}
+mysqli_close($link);
+}
+
+if(!$validUser){ $errorMsg = "Invalid username or password.";}
 else {
 	$_SESSION["login"] = true; 
 	header('Location: ../index.php');
 }
-}
+
 echo $errorMsg;
 ?>
