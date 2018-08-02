f1f87025d53737ca8f136008fe9cfebffe0aab31
dylankite <dylan44117@gmail.com>
2018-08-02 13:31:55 +0000

    Fixed things

--- php/Node.php
+++ php/Node.php
@@ -3,27 +3,37 @@ class Node{
 
   public function firstFloor(){
 
+	  echo "First Floor!";
   $link = mysqli_connect("localhost", "ese","ese");
         $sql = "UPDATE elevator.elevatorNetwork SET 
-                requestedFloor= 1 floor WHERE nodeID=1";
-        mysqli_query($link, $sql);
+                requestedFloor= 1 WHERE nodeID=1";
+
+        $result = mysqli_query($link, $sql);
+	if(!$result){
+	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+		}
+
         mysqli_close($link);
 
 
   }
   public function secondFloor(){
+
+	  echo "Second Floor!";
   $link = mysqli_connect("localhost", "ese","ese");
         $sql = "UPDATE elevator.elevatorNetwork SET 
-                requestedFloor=2 floor WHERE nodeID=1";
+                requestedFloor=2 WHERE nodeID=1";
         mysqli_query($link, $sql);
         mysqli_close($link);
 
 
   }
   public function thirdFloor(){
+
+	  echo "Third Floor!";
   $link = mysqli_connect("localhost", "ese","ese");
         $sql = "UPDATE elevator.elevatorNetwork SET 
-                requestedFloor=3 floor WHERE nodeID=1";
+                requestedFloor=3 WHERE nodeID=1";
         mysqli_query($link, $sql);
         mysqli_close($link);
   }
