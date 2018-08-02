e759be4f9c8da69a378102e23043f071f0d32ea0
Dylan Kite <dylan44117@gmail.com>
2018-08-02 09:54:05 -0400

    to test

--- php/Node.php
+++ php/Node.php
@@ -1,41 +1,47 @@
 <?php
 class Node{
 
-  public function firstFloor(){
 
-	  echo "First Floor!";
-  $link = mysqli_connect("localhost", "ese","ese");
+    private function query_sql($conn, $msg) {
+        $result = mysqli_query($link, $sql);
+	    if(!$result){
+	        throw new Exception ("ERROR: Could not able to execute $sql. " . mysqli_error($link));
+		}
+    }
+
+    public function firstFloor(){
+	    echo "First Floor!";
+        $link = mysqli_connect("localhost", "ese","ese");
         $sql = "UPDATE elevator.elevatorNetwork SET 
                 requestedFloor= 1 WHERE nodeID=1";
 
         $result = mysqli_query($link, $sql);
-	if(!$result){
-	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-		}
-
+	    //if(!$result){
+	    //    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        //}
+        try {
+            query_sql($link, $sql);
+        }
         mysqli_close($link);
-
-
   }
+
   public function secondFloor(){
-
 	  echo "Second Floor!";
-  $link = mysqli_connect("localhost", "ese","ese");
-        $sql = "UPDATE elevator.elevatorNetwork SET 
+      $link = mysqli_connect("localhost", "ese","ese");
+      $sql = "UPDATE elevator.elevatorNetwork SET 
                 requestedFloor=2 WHERE nodeID=1";
-        mysqli_query($link, $sql);
-        mysqli_close($link);
-
-
+      mysqli_query($link, $sql);
+      mysqli_close($link);
   }
-  public function thirdFloor(){
 
-	  echo "Third Floor!";
-  $link = mysqli_connect("localhost", "ese","ese");
+  public function thirdFloor(){
+        echo "Third Floor!";
+        $link = mysqli_connect("localhost", "ese","ese");
         $sql = "UPDATE elevator.elevatorNetwork SET 
                 requestedFloor=3 WHERE nodeID=1";
         mysqli_query($link, $sql);
         mysqli_close($link);
   }
+
 }
 ?>
